5c5fc52418a14ecd6b8afc6ee0c726d08aa988fc
Rahul <rahul.bmninfotech@gmail.com>
2024-06-20 17:54:18 +0530

    Init: inputs

---
+++ src/Components/input.js
@@ -0,0 +1,14 @@
+export default function inputs(colors){
+    const baseStyles = {
+        ".form-text": {
+            width: "100%"
+        }
+    }
+    const generator = (colorName) => {
+
+    }
+
+    return {
+        ...baseStyles,
+    }
+}
--- src/themeComponents.js
+++ src/themeComponents.js
@@ -1,8 +1,12 @@
 import buttons from "./Components/button";
+import inputs from "./Components/input";
 
 function componentStyles(theme){
     const colors = theme('colors');
-    return [buttons(colors)];
+    return [
+        buttons(colors),
+        inputs(colors)
+    ];
 }
 
 export default componentStyles;
